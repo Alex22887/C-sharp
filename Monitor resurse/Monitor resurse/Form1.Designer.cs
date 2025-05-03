e99b883982a48ca0046875588d02a9b385fa5601
Alex22887 <alexandru.tomus2@student.upt.ro>
2025-05-03 13:56:39 +0300

    Add files via upload

---
+++ Monitor resurse/Monitor resurse/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Monitor_resurse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BaraCPU = new System.Windows.Forms.ProgressBar();
+            this.BaraRAM = new System.Windows.Forms.ProgressBar();
+            this.BaraDiskC = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.BaraDiskD = new System.Windows.Forms.ProgressBar();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cpuChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BaraCPU
+            // 
+            this.BaraCPU.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.BaraCPU.Location = new System.Drawing.Point(106, 78);
+            this.BaraCPU.Name = "BaraCPU";
+            this.BaraCPU.Size = new System.Drawing.Size(154, 26);
+            this.BaraCPU.TabIndex = 0;
+            this.BaraCPU.UseWaitCursor = true;
+            // 
+            // BaraRAM
+            // 
+            this.BaraRAM.Location = new System.Drawing.Point(106, 126);
+            this.BaraRAM.Name = "BaraRAM";
+            this.BaraRAM.Size = new System.Drawing.Size(154, 26);
+            this.BaraRAM.TabIndex = 2;
+            this.BaraRAM.UseWaitCursor = true;
+            // 
+            // BaraDiskC
+            // 
+            this.BaraDiskC.Location = new System.Drawing.Point(106, 177);
+            this.BaraDiskC.Maximum = 256;
+            this.BaraDiskC.Name = "BaraDiskC";
+            this.BaraDiskC.Size = new System.Drawing.Size(154, 26);
+            this.BaraDiskC.TabIndex = 3;
+            this.BaraDiskC.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(25, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CPU%";
+            this.label1.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(26, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "RAM%";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(27, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SSD:";
+            this.label4.UseWaitCursor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label8.Location = new System.Drawing.Point(276, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label8";
+            this.label8.UseWaitCursor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label9.Location = new System.Drawing.Point(25, 221);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "HDD:";
+            this.label9.UseWaitCursor = true;
+            // 
+            // BaraDiskD
+            // 
+            this.BaraDiskD.Location = new System.Drawing.Point(106, 221);
+            this.BaraDiskD.Maximum = 1000;
+            this.BaraDiskD.Name = "BaraDiskD";
+            this.BaraDiskD.Size = new System.Drawing.Size(154, 26);
+            this.BaraDiskD.TabIndex = 13;
+            this.BaraDiskD.UseWaitCursor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label10.Location = new System.Drawing.Point(276, 221);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "label10";
+            this.label10.UseWaitCursor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(827, 450);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cpuChart
+            // 
+            this.cpuChart.BackColor = System.Drawing.Color.Transparent;
+            this.cpuChart.BorderlineColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.cpuChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.cpuChart.Legends.Add(legend1);
+            this.cpuChart.Location = new System.Drawing.Point(464, 54);
+            this.cpuChart.Name = "cpuChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.cpuChart.Series.Add(series1);
+            this.cpuChart.Size = new System.Drawing.Size(348, 193);
+            this.cpuChart.TabIndex = 16;
+            this.cpuChart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.cpuChart);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.BaraDiskD);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BaraDiskC);
+            this.Controls.Add(this.BaraRAM);
+            this.Controls.Add(this.BaraCPU);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Monitor Resurse";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar BaraCPU;
+        private System.Windows.Forms.ProgressBar BaraRAM;
+        private System.Windows.Forms.ProgressBar BaraDiskC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ProgressBar BaraDiskD;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cpuChart;
+    }
+}
+
---
+++ Monitor resurse/Monitor resurse/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Monitor_resurse
+{
+    public partial class Form1 : Form
+    {
+        PerformanceCounter CPUcounter;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cpuChart.Series.Clear();
+            cpuChart.ChartAreas[0].AxisX.Title = "Timp";
+            cpuChart.ChartAreas[0].AxisY.Title = "Usage %";
+            Series cpuSeries = new Series("CPU");
+            cpuSeries.ChartType = SeriesChartType.Line;
+            cpuSeries.Color = Color.CornflowerBlue;
+            cpuSeries.BorderWidth = 2;
+            cpuChart.Series.Add(cpuSeries);
+            
+
+
+            CPUcounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+              private void UpdateDiskInfo()
+            {
+                
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+                foreach(DriveInfo d in allDrives)
+                    if(d.IsReady && (d.Name=="C:\\" || d.Name=="D:\\"))
+                    {
+                        string DriveLabel = $"{d.Name} {d.TotalFreeSpace / (1024 * 1024 * 1024):F1} GB liberi din {d.TotalSize / (1024 * 1024 * 1024)} GB";
+                        float UsedDisk = d.TotalSize/(1024*1024*1024)-d.TotalFreeSpace / (1024 * 1024 * 1024);
+
+                        if (d.Name == "C:\\")
+                        {
+                            label8.Text = DriveLabel;
+                            BaraDiskC.Value = (int)UsedDisk;
+                        }
+
+                        if (d.Name == "D:\\")
+                        {
+                            label10.Text = DriveLabel;
+                            BaraDiskD.Value = (int)UsedDisk;
+                        }
+                            
+                    }
+            label1.Parent = pictureBox1;
+            label3.Parent = pictureBox1;
+            label10.Parent = pictureBox1;
+            label4.Parent = pictureBox1;
+            label8.Parent = pictureBox1;
+            label9.Parent = pictureBox1;
+            cpuChart.Parent = pictureBox1;
+            cpuChart.ChartAreas[0].AxisX.LineColor = Color.LightGray;
+            cpuChart.ChartAreas[0].AxisY.LineColor = Color.LightGray;
+            cpuChart.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.White;
+            cpuChart.ChartAreas[0].AxisY.LabelStyle.ForeColor = Color.White;
+            cpuChart.ChartAreas[0].AxisX.TitleForeColor = Color.White;
+            cpuChart.ChartAreas[0].AxisY.TitleForeColor = Color.White;
+            cpuChart.Series["CPU"].Color = Color.LimeGreen;
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float cpuValue = CPUcounter.NextValue();
+            ComputerInfo info = new ComputerInfo();
+            ulong totalMemory = info.TotalPhysicalMemory;
+            ulong availableMemory = info.AvailablePhysicalMemory;
+            ulong usedMemory = totalMemory - availableMemory;
+            int ramUsage = (int)((usedMemory / (double)totalMemory) * 100);
+
+            UpdateDiskInfo();
+
+            BaraCPU.Value = (int)cpuValue;
+            BaraRAM.Value = (int)ramUsage;
+            cpuChart.Series["CPU"].Points.AddY(cpuValue);
+            if (cpuChart.Series["CPU"].Points.Count > 100)
+                cpuChart.Series["CPU"].Points.RemoveAt(0);
+
+            label1.Text = $"CPU: {cpuValue:F1}%";
+            label3.Text = $"RAM: {ramUsage:F1}%";
+        }
+    }
+}
